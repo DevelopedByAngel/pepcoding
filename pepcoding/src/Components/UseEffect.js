db2a149920011a30a326f2cb8f0235fca63e9d3a
Angel Francis <angelfrancis1111@gmail.com>
2021-09-17 11:39:16 +0530

    practice

--- pepcoding/src/Components/UseEffect.js
+++ pepcoding/src/Components/UseEffect.js
@@ -13,13 +13,38 @@ const UseEffect = () => {
 		}
 		},[count,v]);// only when count and v are changed. If no dependencies paramter is specified for all 
 		//if empty [] paramter is specified then only when inital render
+
+
+		const [width,setWidth]=useState(window.innerWidth);
+		// window.addEventListener("resize",()=>setWidth(window.innerWidth));
+		//not outside because code runs for every render and keeps adding number of listeners
+		// useEffect(()=>
+		// 	{
+		// 		window.addEventListener("resize",()=>setWidth(window.innerWidth));
+		// 		// console.log(width+"ls"); 
+		// 	},[]);
+
+
+		//OOOOOOOORRRRRRRRRRRRRRR
+
+		useEffect(()=>
+			{
+				console.log("add");
+				window.addEventListener("resize",()=>setWidth(window.innerWidth));
+				return ()=>
+				{
+					console.log("remove");
+					window.removeEventListener("resize",()=>setWidth(window.innerWidth));
+				}
+				// useEffect can return a cleanup function which will be run first in the next call
+			},[width])
 	return (
 		<div>
 			<h1>{count}</h1>
 			<button onClick={()=>setCount(count+1)}>+</button>
 			<button onClick={()=>sv(v+'!')}>{v}</button>
 			<button onClick={()=>so(o-1)}>-</button>
-
+			<h2>{width}</h2>
 
 		</div>
 		);
